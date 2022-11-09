256d2a2c16037ab08f1898f66cd3cc1074e93ad6
Andrea 'Dre' Arnold <a.arnold_08@yahoo.com>
2022-11-09 16:01:21 -0500

    user and singleUser slices

--- client/store.js
+++ client/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import authReducer from '../../features/auth/authSlice';
+import authReducer from './features/auth/authSlice';
+import usersSlice from './app/slice/usersSlice';
+import singleUserSlice from './app/slice/singleUserSlice';
 
 const store = configureStore({
   reducer: { auth: authReducer },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  usersSlice,
+  singleUserSlice
 });
 
 export default store;
-export * from '../../features/auth/authSlice';
+export * from './features/auth/authSlice';
