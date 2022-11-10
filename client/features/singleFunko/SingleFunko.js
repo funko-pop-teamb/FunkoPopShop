8457e39f50ef014f64e756d9a5906b775b9b235e
Andrea 'Dre' Arnold <a.arnold_08@yahoo.com>
2022-11-10 12:07:05 -0500

    single funko user view

--- client/features/singleFunko/SingleFunko.js
+++ client/features/singleFunko/SingleFunko.js
@@ -3,8 +3,34 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { fetchSingleFunkoPop, selectSimpleFunkoPop } from "../../app/slice/oneFunkoSlice";
+import {
+  fetchSingleFunkoPop,
+  selectSingleFunkoPop,
+} from "../../app/slice/oneFunkoSlice";
 
 const SingleFunko = () => {
+  const dispatch = useDispatch();
+  const { funkoId } = useParams();
 
-}
+  const singleFunko = useSelector(selectSingleFunkoPop);
+
+  const { name, imageUrl,price, category, size, edition, description } = singleFunko;
+
+  useEffect(() => {
+    dispatch(fetchSingleFunkoPop(funkoId));
+  }, [dispatch]);
+
+  return (
+    <div id="single-funko" >
+        <h1>{name}</h1>
+        <img src={imageUrl} />
+        <h4>${price}</h4>
+        <h4>Category:{category}</h4>
+        <h4>Edition:{edition} / Size:{size}</h4>
+        <hr></hr>
+        <h3>About me: {description}</h3>
+        <button type="submit">Add to Cart</button>
+        
+    </div>
+  )
+};
