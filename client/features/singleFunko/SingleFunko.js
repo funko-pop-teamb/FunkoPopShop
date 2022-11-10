49f4890bed3b43c43cc35b120670e53a2bcda39b
Andrea 'Dre' Arnold <a.arnold_08@yahoo.com>
2022-11-10 10:56:32 -0500

    made new branch to work on

---
+++ client/features/singleFunko/SingleFunko.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchSingleFunkoPop, selectSimpleFunkoPop } from "../../app/slice/oneFunkoSlice";
+
+const SingleFunko = () => {
+
+}
