634a12c25cbf80e5f6305bdd2b625f36c0b8697d
ComicJD <comicJLD@gmail.com>
2022-11-10 12:22:57 -0500

    landingpage2

---
+++ client/features/home/LandingPage.js
@@ -0,0 +1,57 @@
+import React,{useState, useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom'
+import { selectFunkoPops, fetchFunkoPops } from '../../app/slice/allFunkoSlice';
+import {fetchFunkoPopByCategory} from '../../app/slice/allFunkoSlice'
+/**
+ * COMPONENT
+ */
+const LandingPage = () => {
+
+    const Funkos = useSelector(selectFunkoPops)
+    const dispatch = useDispatch();
+    const [filteringMethod, setFilteringMethod] = useState('')
+
+
+
+   useEffect(()=>{
+    dispatch(fetchFunkoPops())
+   }, [])
+
+    const filteringMethods=['Disney', 'Movies']
+
+   async function handleFilterChange(category) {
+    console.log('working')
+    setFilteringMethod(category)
+    console.log(fetchFunkoPopByCategory(category))
+     dispatch(fetchFunkoPopByCategory(category))
+    }
+
+
+    
+  console.log(Funkos)
+
+  return (
+<>
+    <div className = "headerHome">
+     <center> <img  src = "https://www.crystalcommerce.com/blog/wp-content/uploads/sites/2/2019/12/billboard_funko-pop-1024x264.jpg" width={1250}/></center>
+    </div>
+
+    <div>
+        <div className='sideNavBar'>
+                    <div onClick={(evt)=> {Funkos.map(Funko)}}> All</div>   
+                    <div onClick={(evt)=> {handleFilterChange('Music Icons')}}> Music Icons</div> 
+                    <div onClick={(evt)=> {handleFilterChange('Disney')}}> Disney</div> 
+                    <div onClick={(evt)=> {handleFilterChange('Movies')}}> Movies</div> 
+                    <div onClick={(evt)=> {handleFilterChange('TV')}}> TV</div> 
+                    <div onClick={(evt)=> {handleFilterChange('DC Comics')}}> DC Comics</div> 
+                    <div onClick={(evt)=> {handleFilterChange('Marvel')}}> Marvel</div> 
+                    <div onClick={(evt)=> {handleFilterChange('Star Wars')}}> Star Wars</div>   
+        </div>
+        <div> </div>
+    </div>
+    </>
+  );
+};
+
+export default LandingPage;
