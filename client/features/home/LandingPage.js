16e2ba2dfa449d2d401398dea184293ed62bb490
ComicJD <comicJLD@gmail.com>
2022-11-10 16:25:56 -0500

    ficed landing page

--- client/features/home/LandingPage.js
+++ client/features/home/LandingPage.js
@@ -8,45 +8,41 @@ import { fetchFunkoPopByCategory } from '../../app/slice/allFunkoSlice'
  */
 const LandingPage = () => {
 
-  const Funkos = useSelector(selectFunkoPops)
-  const dispatch = useDispatch();
-   
+    const Funkos = useSelector(selectFunkoPops)
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(fetchFunkoPops())
+    }, [])
 
-
-  useEffect(() => {
-    dispatch(fetchFunkoPops())
-  }, [])
-
-
-
-  async function handleFilterChange(category) {
-    if (category == 'All'){
-      return  dispatch(fetchFunkoPops())
+    async function handleFilterChange(category) {
+        if (category == 'All') {
+             dispatch(fetchFunkoPops())
+        } else {
+            dispatch(fetchFunkoPopByCategory(category))
+        }
     }
-    dispatch(fetchFunkoPopByCategory(category))
-  }
 
-  return (
-    <>
-      <div className="headerHome">
-        <center> <img src="https://www.crystalcommerce.com/blog/wp-content/uploads/sites/2/2019/12/billboard_funko-pop-1024x264.jpg" width={1250} /></center>
-      </div>
-      <div>
-        <div className='sideNavBar'>
-        <div onClick={(evt) => { handleFilterChange('All') }}> All </div>
-          <div onClick={(evt) => { handleFilterChange('Music Icons') }}> Music Icons</div>
-          <div onClick={(evt) => { handleFilterChange('Disney') }}> Disney</div>
-          <div onClick={(evt) => { handleFilterChange('Movies') }}> Movies</div>
-          <div onClick={(evt) => { handleFilterChange('TV') }}> TV</div>
-          <div onClick={(evt) => { handleFilterChange('DC Comics') }}> DC Comics</div>
-          <div onClick={(evt) => { handleFilterChange('Marvel') }}> Marvel</div>
-          <div onClick={(evt) => { handleFilterChange('Star Wars') }}> Star Wars</div>
-        </div>
-        <div> </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="headerHome">
+                <center> <img src="https://www.crystalcommerce.com/blog/wp-content/uploads/sites/2/2019/12/billboard_funko-pop-1024x264.jpg" width={1250} /></center>
+            </div>
+            <div>
+                <div className='sideNavBar'>
+                    <div onClick={(evt) => { handleFilterChange('All') }}> All </div>
+                    <div onClick={(evt) => { handleFilterChange('Music Icons') }}> Music Icons</div>
+                    <div onClick={(evt) => { handleFilterChange('Disney') }}> Disney</div>
+                    <div onClick={(evt) => { handleFilterChange('Movies') }}> Movies</div>
+                    <div onClick={(evt) => { handleFilterChange('TV') }}> TV</div>
+                    <div onClick={(evt) => { handleFilterChange('DC Comics') }}> DC Comics</div>
+                    <div onClick={(evt) => { handleFilterChange('Marvel') }}> Marvel</div>
+                    <div onClick={(evt) => { handleFilterChange('Star Wars') }}> Star Wars</div>
+                </div>
+                <div> </div>
+            </div>
+        </>
+    );
 };
 
 export default LandingPage;
