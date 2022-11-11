1e7621738f48a4c4613f60a5d220d24578227c0f
Andrea 'Dre' Arnold <a.arnold_08@yahoo.com>
2022-11-11 13:38:33 -0500

    added checkout files

---
+++ client/features/checkout/Checkout.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+const Checkout = () => {
+    
+}
+
+export default Checkout
