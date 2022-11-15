758e05c930adcf07e5b1e295cc12790ea7967f02
Jenny Chen <chen.jenny15@yahoo.com>
2022-11-15 18:40:46 -0500

    guest cart now works

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -12,18 +12,22 @@ import { fetchSingleFunkoPop } from '../../app/slice/oneFunkoSlice'
 const Cart = () => {
     const dispatch = useDispatch();
     // let {totalPrice, shippingAddress}=useSelector((state)=>  {return state.singleOrder.order})
-    const { id, orderStatus, totalPrice } = useSelector((state) => { return state.singleOrder.order })
+    //const { id, orderStatus, totalPrice } = useSelector((state) => { return state.singleOrder.order })
     const items = useSelector((state) => state.cart.items)
     const cartId = useSelector((state) => state.cart.cart.id)
-    const userId= useSelector((state) => state.auth.me.id)
+    const {id} = useSelector((state) => state.auth.me)
 
+    console.log(items)
 
     //const isLoggedIn = useSelector((state) => {return state.auth.me.id})
+    const stored = JSON.parse(localStorage.getItem('cart'))
+    console.log(stored)
 
     // const { userId, cartId } = useParams()
     useEffect(() => {
         // dispatch(filteredOrdersByStatus(userId))
         // dispatch(fetchAllCartFunkos(cartId))
+
     }, [])
 
 
@@ -37,51 +41,70 @@ const Cart = () => {
         return sum
     }
 
-    const removeOne=async (e,item)=>{
+    const removeOne = async (e, item) => {
         e.preventDefault()
-        let orderId= cartId
-        let FunkoPopId=item.FunkoPopId
-        let funkoId=item.FunkoPopId
-        let quantity=item.quantity-1
-        if (item.quantity===1){
-             await dispatch(removeFunkoPop({orderId, funkoId}))
+        let orderId = cartId
+        let FunkoPopId = item.FunkoPopId
+        let funkoId = item.FunkoPopId
+        let quantity = item.quantity - 1
+        if (item.quantity === 1) {
+            await dispatch(removeFunkoPop({ orderId, funkoId }))
         } else {
-           await  dispatch(updateOneOrderOneFunko({orderId, FunkoPopId, quantity}))
+            await dispatch(updateOneOrderOneFunko({ orderId, FunkoPopId, quantity }))
         }
-         await dispatch(fetchAllCartFunkos(orderId))
+        await dispatch(fetchAllCartFunkos(orderId))
     }
-    const removeAll=async (e,item)=>{
+    const removeAll = async (e, item) => {
         e.preventDefault()
-        let orderId= cartId
-        let funkoId=item.FunkoPopId     
-       await dispatch(removeFunkoPop({orderId, funkoId}))
-       await  dispatch(fetchAllCartFunkos(orderId))
+        let orderId = cartId
+        let funkoId = item.FunkoPopId
+        await dispatch(removeFunkoPop({ orderId, funkoId }))
+        await dispatch(fetchAllCartFunkos(orderId))
     }
     return (
         <>
-            <div>Cart
-                <div>
-                    {items && items.length
-                        ? items.map((item) => (
+<div>Cart
+            <div>
+                {!id ?  <div> {stored && stored.length ? stored.map((store) => (
                             <div >
-                                    <img src={item.FunkoPop.imageUrl} className='fImage' />
-                                    <div><Link  to={`/funkoPops/${item.FunkoPop.id}`}> Name: {item.FunkoPop.name}</Link></div>
-                                    {/* <div> funkoPop Id: {item.FunkoPopId}</div> */}
-                                   <button onClick={(e)=>removeOne(e,item)}>remove one</button>
-                                   <button onClick={(e)=>removeAll(e,item)}>remove all</button>
-                                    <div> Qty: {item.quantity}</div>
-                                    <div> Price per item: {item.funkoPrice}</div>
-                                    <div> Total: {item.quantity * item.funkoPrice}</div>
+                                <img src={store.payload.imageUrl} className='fImage' />
+                                <div><Link to={`/funkoPops/${store.payload.id}`}> Name: {store.payload.name}</Link></div>
+                                {/* <div> funkoPop Id: {item.FunkoPopId}</div> */}
+                                <button onClick={(e) => removeOne(e, store)}>remove one</button>
+                                <button onClick={(e) => removeAll(e, store)}>remove all</button>
+                                <div> Qty: {store.quantity}</div>
+                                <div> Price per item: {store.payload.funkoPrice}</div>
+                                <div> Total: {store.payload.quantity * store.payload.funkoPrice}</div>
+                            </div>
+
+                        )) : null
+                        } 
+                        </div> 
+                        
+                        :  
+                        
+                        <div>
+                            {items && items.length ? items.map((item) => (
+                            <div >
+                                <img src={item.FunkoPop.imageUrl} className='fImage' />
+                                <div><Link to={`/funkoPops/${item.FunkoPop.id}`}> Name: {item.FunkoPop.name}</Link></div>
+                                {/* <div> funkoPop Id: {item.FunkoPopId}</div> */}
+                                <button onClick={(e) => removeOne(e, item)}>remove one</button>
+                                <button onClick={(e) => removeAll(e, item)}>remove all</button>
+                                <div> Qty: {item.quantity}</div>
+                                <div> Price per item: {item.funkoPrice}</div>
+                                <div> Total: {item.quantity * item.funkoPrice}</div>
                             </div>
 
                         )) : null
                     }
-                </div>
+                    </div>}
 
+</div>
                 <button><Link to='/funkoPops'>Continue Shopping</Link> </button>
                 <button><Link to='/cart/checkout'>  Check Out</Link></button>
-            </div>
             <div> Cart total:{cartTotal()}</div>
+            </div>
         </>
     )
 }
